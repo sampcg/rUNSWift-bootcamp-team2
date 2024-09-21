124a690d3fd9afb597e21f7f11a7a52cc34e8dd7
Josh Chan <jfusion3600@gmail.com>
2024-09-21 15:33:59 +1000

    Remove test

--- image/home/nao/data/behaviours/body/test/TestArms.py
+++
@@ -1,7 +0,0 @@
-from BehaviourTask import BehaviourTask
-from util.actioncommand import testArms
-
-
-class TestArms(BehaviourTask):
-    def _tick(self):
-        self.world.b_request.actions.body = testArms()
